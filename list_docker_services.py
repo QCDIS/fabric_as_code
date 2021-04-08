ef32194a30c8e189ac38e8d8ace193abd530168d
skoulouzis <skoulouzis@users.noreply.github.com>
2021-04-08 14:57:16 +0200

    Update list_docker_services.py

--- list_docker_services.py
+++ list_docker_services.py
@@ -11,7 +11,7 @@ for docker_service in docker_services:
     service_dict['mode'] = docker_service.attrs['Spec']['Mode']
     service_dict['image'] = docker_service.attrs['Spec']['TaskTemplate']['ContainerSpec']['Image']
     if 'Ports' in docker_service.attrs['Endpoint']:
-        service_dict['ports'] = docker_service.attrs['Endpoint']
+        service_dict['ports'] = docker_service.attrs['Endpoint']['Ports']
     services.append(service_dict)
 
 print(json.dumps(services))
