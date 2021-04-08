221ae562880d0cedc4cf40d392091aedf689887d
skoulouzis <skoulouzis@users.noreply.github.com>
2021-04-08 13:25:22 +0200

    Update list_docker_services.py

--- list_docker_services.py
+++ list_docker_services.py
@@ -2,16 +2,16 @@ import docker
 import json
   
 client = docker.from_env()
-  services = []
-  docker_services = client.services.list()
-  for docker_service in docker_services:
-      service_dict = {}
-      service_dict['name'] = docker_service.name
-      service_dict['id'] = docker_service.short_id
-      service_dict['mode'] = docker_service.attrs['Spec']['Mode']
-      service_dict['image'] = docker_service.attrs['Spec']['TaskTemplate']['ContainerSpec']['Image']
-      if 'Ports' in docker_service.attrs['Endpoint']:
-          service_dict['ports'] = docker_service.attrs['Endpoint']
-      services.append(service_dict)
+services = []
+docker_services = client.services.list()
+for docker_service in docker_services:
+    service_dict = {}
+    service_dict['name'] = docker_service.name
+    service_dict['id'] = docker_service.short_id
+    service_dict['mode'] = docker_service.attrs['Spec']['Mode']
+    service_dict['image'] = docker_service.attrs['Spec']['TaskTemplate']['ContainerSpec']['Image']
+    if 'Ports' in docker_service.attrs['Endpoint']:
+        service_dict['ports'] = docker_service.attrs['Endpoint']
+    services.append(service_dict)
 
-  print(json.dumps(services))
+print(json.dumps(services))
